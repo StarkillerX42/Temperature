571c0e6773822e3ac1db2cc49774d29feac3654e
StarkillerX42 <dgatlin5@gmail.com>
2019-03-07 13:24:22 -0700

    updates

--- get_temp.py
+++ get_temp.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
 import datetime
 import os
 try:
     now = datetime.datetime.now()
-    print("Temperature recorded at {}".format(now))
+    print("Temperature Logger started at at {}".format(now))
     temp = os.popen("/opt/vc/bin/vcgencmd measure_temp").read()
     temp = float(temp.split("=")[1].split("'")[0])
     # print(u"    Temperature: {:.1f}\N{DEGREE SIGN}".format(temp))
@@ -10,6 +11,6 @@ try:
              os.path.abspath(__file__)), "temp_log.txt")
     with open(temppath, "a") as log:
         log.write(u"{},  {:.1f}\n".format(now, temp))
-    print("    Temperature written")
+    print("    Temperature is {:.1f}".format(temp))
 except Exception as e:
     print(e)
--- plot_temp.py
+++ plot_temp.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib as mpl
+mpl.use("Agg")
 import matplotlib.pyplot as plt
 from astropy.time import Time
 times = []
@@ -10,11 +12,25 @@ with open("temp_log.txt", "r") as fil:
         temps.append(float(temp))
 times = np.array(times)
 temps = np.array(temps)
+hot = temps > 80
+print("Temperature is above 80C {:.1f}% of the time".format(
+    np.sum(hot)/len(temps)*100))
+
+
 fig = plt.figure(figsize=(10, 6))
 ax = fig.gca()
 ax.plot_date(times, temps,"o", tz=None,
              xdate=True, ydate=False, drawstyle="steps-post")
 ax.set_title("CPU Temperature Over Time")
 ax.set_xlabel("Date")
-ax.set_ylabel(u"Temperature (\N{DEGREE SIGN})")
+ax.set_ylabel(u"Temperature (\N{DEGREE SIGN}C)")
+ax.axhline(80)
 fig.savefig("temps.png")
+
+fig2 = plt.figure(figsize=(10, 6))
+ax2 = fig2.gca()
+ax2.hist(temps, 50, alpha=0.7, rwidth=0.9)
+ax2.set(xlabel=u"Temperature (\N{DEGREE SIGN}C)", ylabel="Frequency",
+        title="Temperature Histogram")
+ax2.axvline(80)
+fig2.savefig("temp_hist.png")
